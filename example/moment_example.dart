9beab4e47e862c4d6f3a050fc45db5bb9bfb65dc
Rongjian Zhang <pd4d10@gmail.com>
2018-09-08 13:52:08 +0800

    feat: stagehand package-simple

---
+++ example/moment_example.dart
@@ -0,0 +1,6 @@
+import 'package:moment/moment.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/moment.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library moment;
+
+export 'src/moment_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
