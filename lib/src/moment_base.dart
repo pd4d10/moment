79b8acf1512117187db6942c64fd10cac30d1cfa
Rongjian Zhang <pd4d10@gmail.com>
2018-09-15 16:35:43 +0800

    feat: startOf and endOf

--- lib/src/moment_base.dart
+++ lib/src/moment_base.dart
@@ -1,13 +1,21 @@
 import 'package:intl/intl.dart';
 
 enum MomentUnit {
-  second,
-  minute,
-  hour,
+  year,
+  quarter,
+  month,
+  week,
   day,
+  hour,
+  minute,
+  second,
+  millisecond,
+  microsecond,
 }
 
 class Moment {
+  static int monthPerQuarter = 3;
+
   DateTime _dateTime;
 
   Moment([dynamic input]) {
@@ -29,13 +37,6 @@ class Moment {
   Moment.fromMilliseconds(int milliseconds) {
     _dateTime = DateTime.fromMillisecondsSinceEpoch(milliseconds);
   }
-
-  // var mapper = {
-  //   MomentUnit.second: Duration.millisecondsPerSecond,
-  //   MomentUnit.minute: Duration.millisecondsPerMinute,
-  //   MomentUnit.hour: Duration.millisecondsPerHour,
-  //   MomentUnit.day: Duration.millisecondsPerDay
-  // };
 
   Moment add(
       {int years: 0,
@@ -81,6 +82,104 @@ class Moment {
         _dateTime.millisecond - milliseconds,
         _dateTime.microsecond - microseconds);
     return Moment.fromDateTime(dateTime);
+  }
+
+  int get startMonthOfQuarter => _dateTime.month - ((_dateTime.month - 1) % 3);
+  int get startDayOfWeek => _dateTime.day - _dateTime.weekday + 1;
+
+  Moment startOf(MomentUnit unit) {
+    DateTime dateTime;
+    switch (unit) {
+      case MomentUnit.year:
+        dateTime = DateTime(_dateTime.year);
+        break;
+      case MomentUnit.quarter:
+        dateTime = DateTime(_dateTime.year, startMonthOfQuarter);
+        break;
+      case MomentUnit.month:
+        dateTime = DateTime(_dateTime.year, _dateTime.month);
+        break;
+      case MomentUnit.week:
+        dateTime =
+            DateTime(_dateTime.year, startDayOfWeek + DateTime.daysPerWeek);
+        break;
+      case MomentUnit.day:
+        dateTime = DateTime(_dateTime.year, _dateTime.month, _dateTime.day);
+        break;
+      case MomentUnit.hour:
+        dateTime = DateTime(
+            _dateTime.year, _dateTime.month, _dateTime.day, _dateTime.hour);
+        break;
+      case MomentUnit.minute:
+        dateTime = DateTime(_dateTime.year, _dateTime.month, _dateTime.day,
+            _dateTime.hour, _dateTime.minute);
+        break;
+      case MomentUnit.second:
+        dateTime = DateTime(_dateTime.year, _dateTime.month, _dateTime.day,
+            _dateTime.hour, _dateTime.minute, _dateTime.second);
+        break;
+      case MomentUnit.millisecond:
+        dateTime = DateTime(
+            _dateTime.year,
+            _dateTime.month,
+            _dateTime.day,
+            _dateTime.hour,
+            _dateTime.minute,
+            _dateTime.second,
+            _dateTime.millisecond);
+        break;
+      default:
+        return this;
+    }
+    return Moment(dateTime);
+  }
+
+  Moment endOf(MomentUnit unit) {
+    DateTime dateTime;
+    switch (unit) {
+      case MomentUnit.year:
+        dateTime = DateTime(_dateTime.year + 1);
+        break;
+      case MomentUnit.quarter:
+        dateTime =
+            DateTime(_dateTime.year, startMonthOfQuarter + monthPerQuarter);
+        break;
+      case MomentUnit.month:
+        dateTime = DateTime(_dateTime.year, _dateTime.month + 1);
+        break;
+      case MomentUnit.week:
+        dateTime =
+            DateTime(_dateTime.year, startDayOfWeek + DateTime.daysPerWeek);
+        break;
+      case MomentUnit.day:
+        dateTime = DateTime(_dateTime.year, _dateTime.month, _dateTime.day + 1);
+        break;
+      case MomentUnit.hour:
+        dateTime = DateTime(
+            _dateTime.year, _dateTime.month, _dateTime.day, _dateTime.hour + 1);
+        break;
+      case MomentUnit.minute:
+        dateTime = DateTime(_dateTime.year, _dateTime.month, _dateTime.day,
+            _dateTime.hour, _dateTime.minute + 1);
+        break;
+      case MomentUnit.second:
+        dateTime = DateTime(_dateTime.year, _dateTime.month, _dateTime.day,
+            _dateTime.hour, _dateTime.minute, _dateTime.second + 1);
+        break;
+      case MomentUnit.millisecond:
+        dateTime = DateTime(
+            _dateTime.year,
+            _dateTime.month,
+            _dateTime.day,
+            _dateTime.hour,
+            _dateTime.minute,
+            _dateTime.second,
+            _dateTime.millisecond + 1);
+        break;
+      default:
+        return this;
+    }
+    return Moment(dateTime).subtract(microseconds: 1);
   }
 
   String format(String pattern) {
